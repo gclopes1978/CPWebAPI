608bd33efc815284e4b2ec5cfe9b43d84883dfc0
gclopes1978 <gclopes.developer@gmail.com>
2023-05-28 20:17:35 +0100

    Creating the skelleton of the Controllers

---
+++ CPWebAPI/Controllers/District.Controller.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CPWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DistrictsController : ControllerBase
+    {
+        private readonly YourDbContext _context;
+
+        public DistritosController(YourDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Distrito>> GetDistritos()
+        {
+            return _context.Distritos.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Distrito> GetDistrito(int id)
+        {
+            var distrito = _context.Distritos.Find(id);
+
+            if (distrito == null)
+                return NotFound();
+
+            return distrito;
+        }
+    }
+}
--- CPWebAPI/Program.cs
+++ CPWebAPI/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -13,7 +13,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
 
 app.UseHttpsRedirection();
